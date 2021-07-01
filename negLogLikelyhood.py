712741ff91b6fe9180466829be0422f5fbf2d287
Alexande Becker <becker.alexander42@gmail.com>
2021-07-01 23:06:32 +0200

    first experiments with the basic nll

--- negLogLikelyhood.py
+++ negLogLikelyhood.py
@@ -1,6 +1,7 @@
 import numpy as np
 import math
 import matplotlib.pyplot as plt
+import iminuit as imt
 
 
 def nll(pdf, measurements, *theta):
@@ -32,8 +33,8 @@ def nll(pdf, measurements, *theta):
 # an example using the nll to fit a pdf
 # first of all a pdf is needed to generate random numbers with
 # x is a single random vector drawn from X with the pdf 'true_pdf'
-def pdf(x, A, theta1, theta2):
-    return A * math.sin(x-theta1)**2 * np.exp(-(x-theta2)**2)
+def pdf(x, theta1, theta2):
+    return math.sin(x-theta1)**2 * np.exp(-(x-theta2)**2)
 
 # the pdf needs to be normalized, for this an integration is needed, that
 # is performed analytically here
@@ -71,26 +72,48 @@ def generate_arbitrarily_distributed_rngs(pdf, count, domain):
 
 # generate random numbers distributed according to ture_pdf
 domain = (-10, 10)
-area = integrate(pdf, domain, *(1, 1, 1))
-true_pdf = lambda x: pdf(x, 1, 2, 3)
-measurements = generate_arbitrarily_distributed_rngs(true_pdf, 2000, domain)
+theta_1 = 3
+theta_2 = 3
+theta_1_domain = (theta_1-3, theta_1+3)
+theta_2_domain = (theta_2-3, theta_2+3)
+area = integrate(pdf, domain, *(theta_1, theta_2))
+true_pdf = lambda x: pdf(x, theta_1, theta_2) * 1/area
+normed_area = integrate(true_pdf, domain)
+measurements = generate_arbitrarily_distributed_rngs(true_pdf, 20000, domain)
 
 # now we use the nll to show the parameter estimation capability
-theta1 = np.linspace(1, 3, 40)
-theta2 = np.linspace(2, 4, 40)
-theta1, theta2 = np.meshgrid(theta1, theta2)
-likelyhood_scan = np.array([nll(pdf, measurements, *(1/area, t1, t2))
-                            for t1, t2 in zip(theta1.flatten(),
-                                              theta2.flatten())]).reshape(
-                                                      theta1.shape)
-plt.pcolormesh(theta1, theta2, likelyhood_scan, shading='nearest')
-plt.xlabel(r'\theta_1')
-plt.ylabel(r'\theta_2')
-plt.show()
-opt_t1 = theta1.flatten()[np.argmin(likelyhood_scan)]
-opt_t2 = theta2.flatten()[np.argmin(likelyhood_scan)]
+SCAN=False
+if SCAN is True:
+    # the first step is to use a simple (yet computationally intensive) scan
+    # of the parameter space and to use the minimum of the scan as estimate
+    theta1 = np.linspace(theta_1_domain[0], theta_1_domain[1], 60)
+    theta2 = np.linspace(theta_2_domain[0], theta_2_domain[1], 60)
+    # define the grid that will be used for the scan
+    theta1, theta2 = np.meshgrid(theta1, theta2)
+    likelyhood_scan = np.array([nll(pdf, measurements, *(t1, t2))
+                                for t1, t2 in zip(theta1.flatten(),
+                                                  theta2.flatten())]).reshape(
+                                                          theta1.shape)
+    plt.pcolormesh(theta1, theta2, likelyhood_scan, shading='nearest')
+    plt.xlabel(r'$\theta_1$')
+    plt.ylabel(r'$\theta_2$')
+    plt.show()
+    scan_opt_t1 = theta1.flatten()[np.argmin(likelyhood_scan)]
+    scan_opt_t2 = theta2.flatten()[np.argmin(likelyhood_scan)]
+    print(f'Optimal theta_1 determined by scanning = {scan_opt_t1}')
+    print(f'Optimal theta_2 determined by scanning = {scan_opt_t2}')
+
+# now iminuit minimizer is used to find the minimum of the distribution
+# first of all a cost function is needed
+cost_function = lambda theta_1, theta_2: nll(pdf, measurements, *(theta_1, theta_2))
+cost_function.errordef = imt.Minuit.LIKELIHOOD
+m = imt.Minuit(cost_function, theta_1=1, theta_2=1)
+m.simplex().migrad()
+print(f'Optimal values, as determined by iminuit: {tuple(m.values)}')
+scan_opt_t1, scan_opt_t2 = tuple(m.values)
 
 pltx = np.linspace(min(domain), max(domain), 1000)
 plt.hist(measurements, 100, color='blue', alpha=0.1, edgecolor='black', density=True)
-plt.plot(pltx, [pdf(x, 1, opt_t1, opt_t2) for x in pltx])
+plt.plot(pltx, [pdf(x, scan_opt_t1, scan_opt_t2)/area for x in pltx])
+plt.plot(pltx, [true_pdf(x) for x in pltx])
 plt.show()
